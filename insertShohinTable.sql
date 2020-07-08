d5659e92d2e5a894206f7535b405ebe636b54ebb
MasatoKotani629 <masatowww@gmail.com>
2020-07-08 22:50:04 +0900

    insert file

---
+++ insertShohinTable.sql
@@ -0,0 +1,8 @@
+INSERT INTO Shouhin VALUES ('0001', 'Tシャツ', '衣類', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0002', '穴あけパンチ', '事務用品', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0003', 'カッターシャツ', '衣類', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0004', '包丁', '衣類', 'キッチン用品', 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0005', '圧力鍋', 'キッチン用品', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0006', 'フォーク', 'キッチン用品', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0007', 'おろしがね', 'キッチン用品', 1000, 500, '2009-09-20');
+INSERT INTO Shouhin VALUES ('0008', 'ボールペン', '事務用品', 1000, 500, '2009-09-20');
