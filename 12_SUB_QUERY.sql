758f6ad08c42c4a6a4b27fac5cfd6112d5bd2256
MasatoKotani629 <masatowww@gmail.com>
2020-07-26 18:43:40 +0900

    相関サブクエリ

--- 12_SUB_QUERY.sql
+++ 12_SUB_QUERY.sql
@@ -21,3 +21,18 @@ WHERE
 					FROM
 						shohin
 						)
+
+SELECT 
+	shohin_bunrui, shohin_mei, hanbai_tanka
+FROM
+	Shohin As S1
+WHERE
+	hanbai_tanka > (SELECT
+						AVG(hanbai_tanka)
+					FROM
+						Shohin AS S2
+					WHERE 
+						S1.shohin_bunrui = S2.shohin_bunrui
+					GROUP BY 
+						shohin_bunrui
+					); 
