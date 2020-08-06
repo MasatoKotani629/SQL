d6e78261e591cd0db9a136dfcbc1e62aba56b8cc
MasatoKotani629 <masatowww@gmail.com>
2020-08-06 21:08:42 +0900

    BETWEEN

---
+++ 24_BETWEEN.sql
@@ -0,0 +1,21 @@
+SELECT 
+  shohin_mei, hanbai_tanka
+FROM
+  Shohin
+WHERE
+  hanbai_tanka
+BETWEEN
+  100
+AND
+  1000
+;
+
+SELECT 
+  shohin_mei, hanbai_tanka
+FROM
+  Shohin
+WHERE
+  hanbai_tanka >= 100
+AND
+  hanbai_tanka <= 1000
+;
